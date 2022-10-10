70839ec28e3f39547c6660be513e7e6a46999909
JoJoJet <joe102000@gmail.com>
2022-10-10 14:29:26 -0400

    Add examples

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,25 @@
+//! # Examples
+//!
+//! Before:
+//! ```
+//! fn main() -> Result<(), Box<dyn std::error::Error>> {
+//!     let x = some_fallible_operation()?;
+//!     println!("Hello, {x}!");
+//!     Ok(()) // this is ugly
+//! }
+//! # fn some_fallible_operation() -> Result<&'static str, &'static str> { Ok("World") }
+//! ```
+//!
+//! After:
+//! ```
+//! # use tryvial::tryvial;
+//! #[tryvial]
+//! fn main() -> Result<(), Box<dyn std::error::Error>> {
+//!     let x = some_fallible_operation()?;
+//!     println!("Hello, {x}!");
+//! }
+//! # fn some_fallible_operation() -> Result<&'static str, &'static str> { Ok("World") }
+//! ```
+
 pub use tryvial_proc::tryvial;
 
