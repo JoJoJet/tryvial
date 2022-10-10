8dde3c3033c611c1c98832a4640bdc3f84b1869c
JoJoJet <joe102000@gmail.com>
2022-10-10 14:34:03 -0400

    add docs to the attribute macro

--- tryvial-proc/src/lib.rs
+++ tryvial-proc/src/lib.rs
@@ -3,6 +3,11 @@ use proc_macro2::TokenStream as TokenStream2;
 use quote::quote;
 use syn::ItemFn;
 
+/// An attribute macro that performs "Ok-wrapping" on the return value of a `fn` item.
+/// This is compatible with [`Result`], [`Option`], [`ControlFlow`], and any type that
+/// implements the unstable [`std::ops::Try`] trait.
+///
+/// [`ControlFlow`]: core::ops::ControlFlow
 #[proc_macro_attribute]
 pub fn tryvial(_attr: TokenStream, item: TokenStream) -> TokenStream {
     let item = syn::parse_macro_input!(item as ItemFn);
