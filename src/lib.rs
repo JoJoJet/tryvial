276c59e664ed5f6cd02c975cc1b1b6af9b3351a2
JoJoJet <joe102000@gmail.com>
2022-10-10 14:44:58 -0400

    Add a try block macro

--- src/lib.rs
+++ src/lib.rs
@@ -23,3 +23,31 @@
 
 pub use tryvial_proc::tryvial;
 
+/// Performs "Ok-wrapping" on the result of an expression.
+/// This is compatible with [`Result`], [`Option`], [`ControlFlow`], and any type that
+/// implements the unstable [`std::ops::Try`] trait.
+#[macro_export]
+macro_rules! wrap_ok {
+    ($e:expr) => {
+        ::core::iter::empty().try_fold($e, |_, __x: ::core::convert::Infallible| match __x {})
+    };
+}
+
+/// Macro for the receiving end of a `?` operation.
+///
+/// ```
+/// # use tryvial::try_block;
+/// // Note: this fails without explicitly specifying the error type.
+/// let y: Result<_, std::num::ParseIntError> = try_block! {
+///     "1".parse::<i32>()? + "2".parse::<i32>()?
+/// };
+/// # assert_eq!(y, Ok(3));
+/// ```
+#[macro_export]
+macro_rules! try_block {
+    { $($token:tt)* } => {
+        (|| $crate::wrap_ok!({
+            $($token)*
+        }))()
+    }
+}
